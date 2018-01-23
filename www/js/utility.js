d2cf4de4f69ca0dd10e886a1aa5f9c6c2f9c2bdf
BatDr0id <dr1197dr@gmail.com>
2018-01-23 10:29:39 -0500

    Hello
    skjdnhvgfksdhnfv;dsf

--- www/js/utility.js
+++ www/js/utility.js
@@ -1,16 +1,9 @@
-function createPost(id, author, title, content){
-    $('.post-setter').append(
-        "<div class='posts-item' id='"+id+"'>"
-        +"<p id='post-title'>"+ title +"</p>"
-        +"<p id='post-creator'>By: "+ author +"</p>"
-        +"<div id='post-content'>"+ content +"</div></div>");
-}
-
 function getPostData(){
     $.ajax({
         type: "GET", dataType: "json", url: "http://ecommerce18.xyz/wp-json/wp/v2/posts",
         success: function (data){
             getPosts = data;
+            console.log(data);
             //https://stackoverflow.com/questions/13782698/get-total-number-of-items-on-json-object
             for (i =0; i < Object.keys(getPosts).length; i++){
                 var id = getPosts[i].id;
@@ -21,3 +14,11 @@ function getPostData(){
             }
         }});
       };
+
+function createPost(id, author, title, content){
+    $('.post-setter').append(
+        "<div class='posts-item' id='"+id+"'>"
+        +"<p id='post-title'>"+ title +"</p>"
+        +"<p id='post-creator'>By: "+ author +"</p>"
+        +"<div id='post-content'>"+ content +"</div></div>");
+}
