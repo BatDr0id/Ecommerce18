8b9682ac61ea333d6126e9462f33250fbb7c669e
BatDr0id <dr1197dr@gmail.com>
2018-01-23 01:00:10 -0500

    Index and Menu edits
    Adjusted size of item, cleaned up index.html, and moved java script into utility.js
    Working on main menu layout and sizing

--- www/js/utility.js
+++ www/js/utility.js
@@ -5,3 +5,19 @@ function createPost(id, author, title, content){
         +"<p id='post-creator'>By: "+ author +"</p>"
         +"<div id='post-content'>"+ content +"</div></div>");
 }
+
+function getPostData(){
+    $.ajax({
+        type: "GET", dataType: "json", url: "http://ecommerce18.xyz/wp-json/wp/v2/posts",
+        success: function (data){
+            getPosts = data;
+            //https://stackoverflow.com/questions/13782698/get-total-number-of-items-on-json-object
+            for (i =0; i < Object.keys(getPosts).length; i++){
+                var id = getPosts[i].id;
+                var content = getPosts[i].content.rendered;
+                var author = getPosts[i].pwapp_author.name;
+                var title = getPosts[i].title.rendered;
+                createPost(id, author, title, content);
+            }
+        }});
+      };
