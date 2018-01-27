1614bec4c8b7bb122994f4cd84b0ed41ece5807b
BatDr0id <dr1197dr@gmail.com>
2018-01-26 17:30:30 -0800

    Added Menu Updates
    Menu is now javascript created by
    click functionality
    Onclick also works to also close menu
    Added the use of local storage to reduce loading time and network usage

--- www/js/utility.js
+++ www/js/utility.js
@@ -1,4 +1,5 @@
 var hurl = "https://ecommerce18.xyz";
+
 function getPostData(){
     $.ajax({
         type: "GET", dataType: "json", url: hurl + "/wp-json/wp/v2/posts?fields=pwapp_author,content,title",
@@ -12,9 +13,7 @@ function getPostData(){
                 var author = getPosts[i].pwapp_author.name;
                 var title = getPosts[i].title.rendered;
                 createPost(id, author, title, content);
-            }
-        }});
-      };
+}}});}
 
 function createPost(id, author, title, content){
     $('.post-setter').append(
@@ -22,21 +21,55 @@ function createPost(id, author, title, content){
         +"<p id='post-title'>"+ title +"</p>"
         +"<p id='post-creator'>By: "+ author +"</p>"
         +"<div id='post-content'>"+ content +"</div></div>");
-};
+}
+function createMenu(){
+    $(".main").append(
+    "<div class='navMenuContainer'>"+
+    "<nav class='navMenu'>" +
+    "<ul><li><a href=''>Home</a></li>"+
+    "<li><a href=''>Shop</a></li>"+
+    "<li><a href=''>My Account</a></li>"+
+    "</ul><div class='divider'></div>"+
+    "<ul class='productMenu'></ul>"
+    );
+    if (localStorage.getItem("product_menu") !== 'undefined'){
+        pmArray = JSON.parse(localStorage.getItem("product_menu"));
+        for(i = 0; i < Object.keys(pmArray).length;i++){
+            var name = pmArray[i][0];
+            var slug = pmArray[i][1];
+            var link = pmArray[i][2];
+            createProductMenu(name, slug);
+        }
+    }
+    else {
+        getProductMenu();
+    }
+    $(".navMenuContainer").click(function(){
+        $(".navMenuContainer").empty().remove();
+});}
 function getProductMenu(){
     var zero = 0;
-                $.ajax({
-                    type: "GET", dataType: "json",
-                    url: "https://ecommerce18.xyz/wp-json/wc/v2/products/categories?consumer_key=ck_a88c9def695ef2c8accd8a04393f54d79b55890f&consumer_secret=cs_1aa3c7676c58e4a54ecf58db18fa7f6fd3d0ab3d",
-                    success: function (data){
-                    menu = data;
-                    console.log(menu);
+    var pmArray = [];
+     if (localStorage.getItem("product_menu") == null){
+    $.ajax({
+        type: "GET", dataType: "json",
+        url: "https://ecommerce18.xyz/wp-json/wc/v2/products/categories?consumer_key=ck_a88c9def695ef2c8accd8a04393f54d79b55890f&consumer_secret=cs_1aa3c7676c58e4a54ecf58db18fa7f6fd3d0ab3d",
+        success: function (data){
+            menu = data;
+            for(i = 0; i < Object.keys(menu).length;i++){
+                var name = menu[i].name;
+                var slug = menu[i].slug;
+                var link = menu[i]._links.self[0].href;
+                var tmp = [name, slug, link];
+                pmArray.push(tmp);
+            }//https://stackoverflow.com/questions/13782698/get-total-number-of-items-on-json-object
+            localStorage.setItem("product_menu", JSON.stringify(pmArray));
+                console.log(localStorage);
 
-                for(i = 0; i < Object.keys(menu).length;i++){
-                    var name = menu[i].name;
-                    var link = menu[i]._links.self[0].href;
-                    console.log(link);
-                }
-            //https://stackoverflow.com/questions/13782698/get-total-number-of-items-on-json-object
-       }});
-};
+        }
+})}};
+
+function createProductMenu(name, slug){
+    $('.productMenu').append(
+        "<li><a href='' slug='"+ slug +"'>"+ name +"</a></li>");
+}
