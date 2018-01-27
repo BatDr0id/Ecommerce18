926cdac61d26a34ace078443c268aa1ae647e495
BatDr0id <dr1197dr@gmail.com>
2018-01-27 15:59:54 -0800

    Merge branch 'layout'

--- www/js/utility.js
+++ www/js/utility.js
@@ -22,6 +22,15 @@ function createPost(id, author, title, content){
         +"<p id='post-creator'>By: "+ author +"</p>"
         +"<div id='post-content'>"+ content +"</div></div>");
 }
+/*function createProduct(id, img, name, price, description, linkToProduct )
+{
+    $('.post-setter').append(
+        "<div class='posts-item' id='"+id+"'>"
+        +"<img src =" + img + ">"
+        +"<p id='post-title'> <a href=" + linkToProduct + ">" name + "</a></p>"
+        +"<p id='post-creator'>Price: "+ price +"</p>"
+        +"<div id='post-content'>"+ description +"</div></div>");
+}*/
 function createMenu(){
     $(".main").append(
     "<div class='navMenuContainer'>"+
